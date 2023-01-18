edc3dde1e77b36562c1567d4cbe3a94bcb1fd9db
DESKTOP-RCJOS5S\sangkook <sixstar6@naver.com>
2023-01-19 05:36:39 +0900

    업무별 이슈 관리를 위한 도메인 init

---
+++ src/main/java/com/sk/manage/domain/issue/Tag.java
@@ -0,0 +1,5 @@
+package com.sk.manage.domain.issue;
+
+public class Tag {
+
+}
---
+++ src/test/java/com/sk/manage/domain/issue/IssueTest.java
@@ -0,0 +1,12 @@
+package com.sk.manage.domain.issue;
+
+import org.junit.jupiter.api.Test;
+
+class IssueTest {
+
+	@Test
+	void test() {
+		
+	}
+
+}
